779ae0bc4f79fb575873541bbbd358b0be016e00
Lameck <lameck2@hotmail.com>
2019-07-15 20:14:43 -0300

    Add graphql Hello world

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,45 @@
-// Importing stuff
-import express from 'express';
-
-// Importing dotenv file
 require('dotenv').config();
+import express, { Response } from 'express';
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 import applyConfigs from './config';
 
-// Create new instance from express
 const app = express();
 
 // Test Route
-app.get('/', (_, res) => {
-	res.send('Hello ts-node!');
+app.get('/', (_, res: Response) => {
+  res.send('Hello ts-node!');
 });
 
 // Apply configs
 applyConfigs(app);
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  }
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-	console.log(`[SERVER] Running at http://localhost:${PORT}`);
+  console.log(`[SERVER] Running at http://localhost:${PORT}`);
 });
 
 // Exporting server (this is for tests)
