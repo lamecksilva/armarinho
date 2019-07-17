b919eb53be0bd56bb92a5537142c3c84e3bedada
Santos <lameck.santos@capgemini.com>
2019-07-17 10:29:12 -0300

    feat: add str and user types in graphql schema

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
-import express, { Response } from 'express';
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+import express, { Response, Request } from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 import applyConfigs from './config';
 
@@ -9,37 +9,77 @@ const app = express();
 
 // Test Route
 app.get('/', (_, res: Response) => {
-  res.send('Hello ts-node!');
+	res.send('Hello ts-node!');
 });
 
 // Apply configs
 applyConfigs(app);
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+let str = 'Hello World';
+let userObject = {
+	name: 'Lamec',
+	age: 18
+};
+
+const graphQlSchema = buildSchema(`
+	type User {
+		name: String!
+		age: Int!
+		email: String
+	}
+
+  type RootQuery {
+		str: String!	
+		user: User!
+	}
+
+	type RootMutation {
+		changeStr(newStr: String): String
+		changeUserData(newUser: User): User
+	}
+
+	schema {
+		query: RootQuery
+		mutation: RootMutation
+	}
 `);
 
 const root = {
-  hello: () => {
-    return 'Hello world!';
-  }
+	changeStr: async (args: any, req: Request) => {
+		str = await args.newStr;
+		console.log(req.headers.authorization);
+
+		return str;
+	},
+	str: () => {
+		return str;
+	},
+	user: () => {
+		return userObject;
+	},
+	changeUserData: async (args: any, req: Request) => {
+		userObject = await args.newUser;
+
+		console.log(req.headers.authorization);
+		console.log(args);
+
+		return userObject;
+	}
 };
 
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-  })
+	'/graphql',
+	graphqlHTTP({
+		schema: graphQlSchema,
+		rootValue: root
+		// graphiql: true
+	})
 );
 
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-  console.log(`[SERVER] Running at http://localhost:${PORT}`);
+	console.log(`[SERVER] Running at http://localhost:${PORT}`);
 });
 
 // Exporting server (this is for tests)
