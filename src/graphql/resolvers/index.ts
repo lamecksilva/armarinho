14f8afe07ffce94d6d158b865ac06304af271468
lamecksilva <lameck2@hotmail.com>
2019-07-19 16:18:10 -0300

    fix: Delete files

--- src/graphql/resolvers/index.ts
+++
@@ -1,8 +0,0 @@
-// import user from './user.resolver';
-
-// // Exporting all keys (functions/resolvers) in one object
-// export default {
-// 	...user
-// };
-
-// // export * from './user.resolver';
