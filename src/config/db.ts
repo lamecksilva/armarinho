8d7e847d164cbc0f9033b47d370ef9f028becda7
Lameck <lameck2@hotmail.com>
2019-07-20 12:51:59 -0300

    fix: Remove password field from User type; feat: Update npm scripts to auto reload

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,32 +2,32 @@ import mongoose from 'mongoose';
 
 // Function to connect app to MongoDB
 export default (): void => {
-	const MONGO_URL =
-		process.env.MONGO_URL || 'mongo://localhost:27017/armarinho';
+  const MONGO_URL =
+    process.env.MONGO_URL || 'mongodb://localhost:27017/armarinho';
 
-	if (!process.env.MONGO_URL) {
-		throw new Error('MONGO_URL não declarada');
-	}
+  if (!MONGO_URL) {
+    throw new Error('MONGO_URL não declarada');
+  }
 
-	mongoose.connect(MONGO_URL, { useNewUrlParser: true });
-	const db = mongoose.connection;
+  mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+  const db = mongoose.connection;
 
-	// Handling events
-	db.on('error', err => {
-		console.error('Erro na conexão com o MongoDB: ');
-		throw new Error(err);
-	});
+  // Handling events
+  db.on('error', err => {
+    console.error('Erro na conexão com o MongoDB: ');
+    throw new Error(err);
+  });
 
-	db.once('open', () => {
-		console.info('MongoDB conectado com sucesso!');
-	});
+  db.once('open', () => {
+    console.info('MongoDB conectado com sucesso!');
+  });
 
-	db.on('disconnected', () => {
-		console.error('MongoDB desconectado');
-		mongoose.connect(MONGO_URL, { server: { auto_reconnect: true } });
-	});
+  db.on('disconnected', () => {
+    console.error('MongoDB desconectado');
+    mongoose.connect(MONGO_URL, { server: { auto_reconnect: true } });
+  });
 
-	db.on('reconnected', () => {
-		console.info('MongoDB Reconectado!');
-	});
+  db.on('reconnected', () => {
+    console.info('MongoDB Reconectado!');
+  });
 };
--- src/graphql/users/type.ts
+++ src/graphql/users/type.ts
@@ -10,33 +10,29 @@
  */
 
 import {
-	GraphQLObjectType,
-	GraphQLString,
-	GraphQLNonNull,
-	GraphQLID
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLID
 } from 'graphql';
 
 // import Resolver from './resolvers';
 
 export default new GraphQLObjectType({
-	description: 'A User',
-	fields: () => ({
-		_id: {
-			description: "User's ID",
-			type: GraphQLID
-		},
-		email: {
-			description: "The user's email",
-			type: GraphQLString
-		},
-		name: {
-			description: "The user's name",
-			type: new GraphQLNonNull(GraphQLString)
-		},
-		password: {
-			description: "The user's account password",
-			type: new GraphQLNonNull(GraphQLString)
-		}
-	}),
-	name: 'User'
+  description: 'A User',
+  fields: () => ({
+    _id: {
+      description: "User's ID",
+      type: GraphQLID
+    },
+    email: {
+      description: "The user's email",
+      type: GraphQLString
+    },
+    name: {
+      description: "The user's name",
+      type: new GraphQLNonNull(GraphQLString)
+    }
+  }),
+  name: 'User'
 });
