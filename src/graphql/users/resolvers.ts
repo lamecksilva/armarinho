0fd6d88fac8db71beb392cca223982dc27f141d8
Lameck <lameck2@hotmail.com>
2019-07-21 11:14:21 -0300

    fix: Remove password field from User GraphQL type

--- src/graphql/users/resolvers.ts
+++ src/graphql/users/resolvers.ts
@@ -1,9 +1,9 @@
 import { getUsers, saveUser } from './services';
 
 export default {
-	// Create a user
-	createUser: (_: any, props: any) => saveUser(props),
+  // Create a user
+  createUser: (_: any, props: any) => saveUser(props),
 
-	// Get all users or find one
-	users: (_: any, props: any) => getUsers(props)
+  // Get all users or find one
+  users: (_: any, props: any) => getUsers(props)
 };
--- src/graphql/users/type.ts
+++ src/graphql/users/type.ts
@@ -4,7 +4,6 @@
  * 		id: String!
  *    name: String!
  *    email: String!
- *    password: String!
  * }
  *
  */
@@ -16,18 +15,16 @@ import {
   GraphQLID
 } from 'graphql';
 
-// import Resolver from './resolvers';
-
 export default new GraphQLObjectType({
   description: 'A User',
   fields: () => ({
     _id: {
       description: "User's ID",
-      type: GraphQLID
+      type: new GraphQLNonNull(GraphQLID)
     },
     email: {
       description: "The user's email",
-      type: GraphQLString
+      type: new GraphQLNonNull(GraphQLString)
     },
     name: {
       description: "The user's name",
