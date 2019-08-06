9ead680e00e1e75f0710a584f25535ecca19955e
lamecksilva <lameck2@hotmail.com>
2019-08-06 09:02:40 -0300

    fix: Remove try catches blocks
    Signed-off-by: lamecksilva <lameck2@hotmail.com>

--- user-service/src/graphql/users/services.ts
+++ user-service/src/graphql/users/services.ts
@@ -10,92 +10,73 @@ import jwtSign from '../../utils/jwt-sign';
 
 // ================================ Get all users from DB ===============================
 export const getUsers = async (props: any) => {
-	try {
-		let users;
+	let users;
 
-		props.id !== undefined
-			? (users = await User.find({ _id: props.id }))
-			: (users = await User.find({}));
+	props.id !== undefined
+		? (users = await User.find({ _id: props.id }))
+		: (users = await User.find({}));
 
-		props.email !== undefined
-			? (users = await User.find({ email: props.email }))
-			: (users = await User.find({}));
+	props.email !== undefined
+		? (users = await User.find({ email: props.email }))
+		: (users = await User.find({}));
 
-		return users.map((u: any) => {
-			return { ...u._doc, password: null };
-		});
-	} catch (e) {
-		console.error('Erro na função getUsers');
-		console.error(e);
-
-		throw new Error(e);
-	}
+	return users.map((u: any) => {
+		return { ...u._doc, password: null };
+	});
 };
 
 // ================================ Creates a new user ====================================
 export const saveUser = async (props: CreateUserInput) => {
-	try {
-		const { name, email, password } = props;
+	const { name, email, password } = props;
 
-		// Create a new User from Model
-		const newUser = await new User({
-			name,
-			email,
-			password: await hash(password, 10)
-		});
+	// Create a new User from Model
+	const newUser = await new User({
+		name,
+		email,
+		password: await hash(password, 10)
+	});
 
-		// Saving in the database
-		const result = await newUser.save();
+	// Saving in the database
+	const result = await newUser.save();
 
-		// return created user
-		return { ...result._doc, password: null };
-	} catch (e) {
-		console.log('Erro na funçao saveUser');
-
-		throw new Error(e);
-	}
+	// return created user
+	return { ...result._doc, password: null };
 };
 
 // =============================== Edit and save user ====================================
 export const editUser = async ({ id, email, name }: EditUserInput) => {
-	try {
-		// Validate input data
-		let { isValid, errors } = validateEditUserInput({ id, email, name });
+	// Validate input data
+	let { isValid, errors } = validateEditUserInput({ id, email, name });
 
-		if (!isValid) {
-			return {
-				user: null,
-				errors
-			};
-		}
-
-		if (!(await User.findOne({ _id: id }))) {
-			errors.push({
-				key: 'id',
-				message: 'ID não encontrado no banco de dados'
-			});
-
-			return { user: null, errors };
-		}
-
-		const userUpdated: any = await User.findOneAndUpdate(
-			{ _id: id },
-			{ $set: { email, name } },
-			{ new: true }
-		);
-
+	if (!isValid) {
 		return {
-			user: {
-				...userUpdated._doc,
-				password: null
-			},
+			user: null,
 			errors
 		};
-	} catch (e) {
-		console.log(e);
-
-		throw new Error(e);
 	}
+
+	if (!(await User.findOne({ _id: id }))) {
+		errors.push({
+			key: 'id',
+			message: 'ID não encontrado no banco de dados'
+		});
+
+		return { user: null, errors };
+	}
+
+	const userUpdated: any = await User.findOneAndUpdate(
+		{ _id: id },
+		{ $set: { email, name } },
+		{ new: true }
+	);
+
+	return {
+		user: {
+			...userUpdated._doc,
+			password: null
+		},
+		errors
+	};
 };
 
 // ================================ Login user =================================
@@ -146,14 +127,9 @@ export const generateJwtToken = async (props: any) => {
 
 // ================================ Delete user ================================
 export const removeUser = async (props: any) => {
-	try {
-		let user: any;
+	let user: any;
 
-		user = await User.findOneAndDelete({ _id: props.id });
+	user = await User.findOneAndDelete({ _id: props.id });
 
-		return { ...user._doc, password: null };
-	} catch (e) {
-		throw new Error(e);
-		console.log('Erro na função removeUser');
-	}
+	return { ...user._doc, password: null };
 };
