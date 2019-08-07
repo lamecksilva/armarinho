7ff89c0da032c34b34c1dee3ff779a393cdc1d1d
lamecksilva <lameck2@hotmail.com>
2019-08-07 15:31:42 -0300

    feat: Update routes urls in API-Gateway

--- api-gateway/index.ts
+++ api-gateway/index.ts
@@ -1,5 +1,5 @@
 import http from 'http';
-import express, { Response } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import httpProxy from 'express-http-proxy';
 import helmet from 'helmet';
 import morgan from 'morgan';
@@ -8,17 +8,17 @@ import isReachable from 'is-reachable';
 
 const app = express();
 
-const userServiceProxy = httpProxy('http://localhost:9001');
-const productServiceProxy = httpProxy('http://localhost:9002');
+const authServiceProxy = httpProxy('http://localhost:9001');
+const userServiceProxy = httpProxy('http://localhost:9002');
 
 app.get('/', (_, res: Response) => res.send('Hello from API-Gateway'));
 
-app.use('/users', (req, res, next) => {
-	userServiceProxy(req, res, next);
+app.use('/auth', (req: Request, res: Response, next: NextFunction) => {
+	authServiceProxy(req, res, next);
 });
 
-app.use('/products', (req, res, next) => {
-	productServiceProxy(req, res, next);
+app.use('/users', (req: Request, res: Response, next: NextFunction) => {
+	userServiceProxy(req, res, next);
 });
 
 app.use(morgan('dev'));
@@ -28,36 +28,45 @@ app.use(express.urlencoded({ extended: false }));
 
 // HealthCheck
 app.get('/status', async (_, res: Response) => {
-	const userService = 'http://localhost:9001';
-	const productService = 'http://localhost:9002';
-	const orderService = 'http://localhost:9003';
-	const filesService = 'http://localhost:9004';
-	const notificationService = 'http://localhost:9005';
-	const paymentService = 'http://localhost:9006';
+	const authService = 'http://localhost:9001';
+	const userService = 'http://localhost:9002';
+	const productService = 'http://localhost:9003';
+	const orderService = 'http://localhost:9004';
+	const filesService = 'http://localhost:9005';
+	const notificationService = 'http://localhost:9006';
+	const paymentService = 'http://localhost:9007';
 
 	res.json({
-		userService: (await isReachable(userService, { timeout: 500 }))
+		authService: (await isReachable(authService, { timeout: 300 }))
 			? 'Online'
 			: 'Offline',
-		productService: (await isReachable(productService, { timeout: 500 }))
+		userService: (await isReachable(userService, { timeout: 300 }))
 			? 'Online'
 			: 'Offline',
-		orderService: (await isReachable(orderService, { timeout: 500 }))
+		productService: (await isReachable(productService, { timeout: 300 }))
 			? 'Online'
 			: 'Offline',
-		filesService: (await isReachable(filesService, { timeout: 500 }))
+		orderService: (await isReachable(orderService, { timeout: 300 }))
+			? 'Online'
+			: 'Offline',
+		filesService: (await isReachable(filesService, { timeout: 300 }))
 			? 'Online'
 			: 'Offline',
 		notificationService: (await isReachable(notificationService, {
-			timeout: 500
+			timeout: 300
 		}))
 			? 'Online'
 			: 'Offline',
-		paymentService: (await isReachable(paymentService, { timeout: 500 }))
+		paymentService: (await isReachable(paymentService, { timeout: 300 }))
 			? 'Online'
 			: 'Offline'
 	});
 });
 
 const server = http.createServer(app);
-server.listen(9000);
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () =>
+	console.log(`API-Gateway running on http://localhost:${PORT}`)
+);
--- auth-service/index.ts
+++ auth-service/index.ts
@@ -11,11 +11,19 @@ const app = express();
 
 app.use(express.json());
 
-// Test Route
+/**
+ *
+ * Test Route
+ *
+ */
 app.get('/', (_, res: Response) => res.send('Hello from AUTH-service'));
 
-// Login route
-app.post('/auth/login', async (req: Request, res: Response) => {
+/**
+ *
+ *  Login route
+ *
+ */
+app.post('/login', async (req: Request, res: Response) => {
 	// GraphQL Query
 	const query = `query ($email: String!, $password: String!) {
 			login(email: $email, password: $password) {
@@ -34,47 +42,63 @@ app.post('/auth/login', async (req: Request, res: Response) => {
 		req.body
 	);
 
-	let { errors } = data.login;
+	let { errors, token } = data.login;
 
 	if (errors.length) {
 		return res.status(400).json({ errors });
 	}
-
-	const { token } = data.login;
-	const tokenType = 'Bearer';
-	const refreshExpiresIn = '7h';
-	const expiresIn = '1h';
 
 	const refreshToken = await jwtSign(
 		{ provisoryItem: 'Provisory Value', provisoryItem2: 'Provisory Value 2' },
 		'secret'
 	);
 
-	return res
-		.status(200)
-		.json({ tokenType, token, expiresIn, refreshToken, refreshExpiresIn });
+	return res.status(200).json({
+		tokenType: 'Bearer',
+		token,
+		expiresIn: '1h',
+		refreshToken,
+		refreshExpiresIn: '7h'
+	});
 });
 
-// Route to test Authorization http header
+/**
+ *
+ * Route to test Authorization http header
+ *
+ */
 app.get('/protected', async (req: Request, res: Response) => {
 	const token: string = req.headers.authorization || 'noToken';
 
 	// Verify jwt token
 	await jwtVerify(token, 'secret')
 		.then(isValid => {
-			console.log(isValid);
-
-			return res.status(200).json({ success: true, message: 'JWT Válido' });
+			return res.status(200).json({ success: isValid, message: 'JWT Válido' });
 		})
 		.catch(err => {
-			console.error(err);
-
-			return res
-				.status(401)
-				.json({ success: false, error: { message: 'JWT Token inválido' } });
+			return res.status(401).json({
+				success: err != undefined,
+				error: { message: 'JWT Token inválido' }
+			});
 		});
 
+	// If returns before verify
 	return res.status(102).json({ success: true });
+});
+
+/**
+ *
+ * Route to refresh token
+ *
+ */
+app.post('/refresh-token', async (req: Request, res: Response) => {
+	// 1 -> Check if refreshToken is valid
+	// 2 -> Generate an new AcessToken
+	// 3 -> Generate an new RefreshToken
+	// 4 -> Return new tokens
+	console.log(req.body);
+
+	return res.status(200);
 });
 
 const PORT = process.env.PORT || 9001;
