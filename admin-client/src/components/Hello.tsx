cd690d5ebe095ae5d6bb9eddb52746113ca2f928
Lameck <lameck2@hotmail.com>
2019-08-19 22:26:49 -0300

    feat: Add State

--- admin-client/src/components/Hello.tsx
+++ admin-client/src/components/Hello.tsx
@@ -1,19 +1,38 @@
 import * as React from 'react';
 
+//'HelloProps' describes the shape of props.
 export interface HelloProps {
 	projectName?: string;
 }
 
-// export interface HelloState {}
+//'HelloState' describes the shape of the state.
+export interface HelloState {
+	count: number;
+}
 
-//'HelloProps' describes the shape of props.
-//'HelloState' describes the shape of props.
-export class Hello extends React.Component<HelloProps> {
+export class Hello extends React.Component<HelloProps, HelloState> {
+	constructor(props: HelloProps) {
+		super(props);
+
+		this.state = { count: 0 };
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange = (e: React.MouseEvent) => {
+		e.preventDefault();
+
+		this.setState({
+			count: this.state.count + 1
+		});
+	};
+
 	render(): JSX.Element {
 		return (
 			<div>
 				<h1>Hello from {this.props.projectName}</h1>
-				<h3>Successfully setup our React, Webpack, and Typescript project!</h3>
+				<h3>Counter : {this.state.count}</h3>
+				<button onClick={this.handleChange}>Click Me!</button>
 			</div>
 		);
 	}
