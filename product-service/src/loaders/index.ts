7338df5e118787563ef519a4f86fc17e838a96fb
lamecksilva <lameck2@hotmail.com>
2019-09-12 16:38:01 -0300

    feat: Sequelize Connection Established

--- product-service/src/loaders/index.ts
+++ product-service/src/loaders/index.ts
@@ -1,8 +1,15 @@
 import { Application } from 'express';
 
 import expressLoader from './express.loader';
+import sequelizeLoader from './sequelize.loader';
 
 export default async (expressApp: Application) => {
 	await expressLoader(expressApp);
 	console.info('Express Initialized');
+
+	const sequelizeConnection = await sequelizeLoader();
+	sequelizeConnection
+		.authenticate()
+		.then(() => console.log('MYSQL Connected!!'))
+		.catch(err => console.error(err));
 };
--- product-service/src/loaders/sequelize.loader.ts
+++ product-service/src/loaders/sequelize.loader.ts
@@ -1,23 +1,22 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize({
-	host: 'localhost',
-	port: 3306,
-	database: 'armarinho_products_DB',
-	username: 'lameck',
-	password: 'GUERRAcivil',
-	dialect: 'mysql',
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000
-	}
-});
+const sequelizeLoader = async () => {
+	const sequelize = new Sequelize({
+		host: 'localhost',
+		port: 3306,
+		database: 'armarinho_products_DB',
+		username: 'lameck',
+		password: 'GUERRAcivil',
+		dialect: 'mysql',
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000
+		}
+	});
 
-sequelize
-	.authenticate()
-	.then(() => console.log('MYSQL Connected!!'))
-	.catch(err => console.error(err));
+	return sequelize;
+};
 
-export default sequelize;
+export default sequelizeLoader;
